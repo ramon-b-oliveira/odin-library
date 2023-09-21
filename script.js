f03a2857752d4d2199354abc3ca8cb2e31f5ec32
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-09-21 15:00:48 -0400

    implement css and js for sidebar

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const inputTitle = document.querySelector('#title');
+const inputAuthor = document.querySelector('#author');
+const inputRead = document.querySelector('#read');
+const addButton = document.querySelector('.addbutton');
 const myLibrary = [];
 
 function Book(title, author, read) {
@@ -6,7 +10,17 @@ function Book(title, author, read) {
   this.read = Boolean(read);
 }
 
-function addBookToLibrary() {
-  const newBook = new Book(inputTitle, inputAuthor, inputRead)
-  myLibrary.push(newBook)
+function addBookToLibrary(event) {
+  let read = false
+  if (inputRead.checked) {
+    read = true
+  }
+  const newBook = new Book(inputTitle.value, inputAuthor.value, read);
+  myLibrary.push(newBook);
+  event.preventDefault();
+  inputTitle.value = "";
+  inputAuthor.value = "";
+  inputRead.checked = false;
 }
+
+addButton.addEventListener("click", addBookToLibrary)
